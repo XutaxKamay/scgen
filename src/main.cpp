bbc6724e0de824b81c8996c3b16ee629ffeda8a6
Kamay Xutax <admin@xutaxkamay.com>
2022-10-01 06:34:55 +0200

    Make main inlined

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include "pch.h"
-#include "view_as.h"
 
-void main()
+inline void main()
 {
-    volatile const char hello_world[] = "Hello, World\n";
+    const char hello_world[] = "Hello, World\n";
+
     kernel::syscall(__NR_write,
                     STDOUT_FILENO,
                     hello_world,
                     sizeof(hello_world));
 
     kernel::syscall(__NR_exit, 0);
+
+    /**************************
+     * Should not reach here. *
+     **************************/
+    asm volatile("int3");
 }
 
 extern "C" void _start()
--- src/view_as.h
+++ src/view_as.h
@@ -2,7 +2,7 @@
 #define SHELLCODE_VIEW_AS_H
 
 template <typename T>
-constexpr auto view_as(const auto var)
+inline constexpr auto view_as(const auto var)
 {
     return (T)(var);
 }
